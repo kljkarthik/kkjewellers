d777f40e198b318cb0d683421e42650d786cc5f5
klj karthik <kljkarthik79@gmail.com>
2026-08-29 14:12:41 +0530

    build: update compiled production distribution bundle

---
+++ frontend/dist/assets/leadService-C8c7on0q.js
@@ -0,0 +1,1 @@
+import{A as t}from"./index-D_eWpwzN.js";const e=async s=>(await t.post("/enquiries",s)).data,a=async s=>(await t.post("/appointments",s)).data;export{a as b,e as s};
--- frontend/dist/assets/leadService-UAMlaNeI.js
+++
@@ -1,1 +0,0 @@
-import{A as t}from"./index-K5W4eilf.js";const e=async s=>(await t.post("/enquiries",s)).data,a=async s=>(await t.post("/appointments",s)).data;export{a as b,e as s};
